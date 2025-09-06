ffa67517882999a4966466cb3cd3a87897e8bb77
flypapercut <flypapercut@proton.me>
2025-09-06 16:57:34 -0300

    feat(api): add indexing seeds functionality

--- src/controllers/seeds.controller.ts
+++ src/controllers/seeds.controller.ts
@@ -1,11 +1,13 @@
 import { Request, Response, NextFunction } from "express";
 import { knexConfig as knex } from "../database/knex";
+import { Seed } from "../models/Seed";
 
 class SeedsController {
 	async index(request: Request, response: Response, next: NextFunction) {
 		try {
 			console.log("indexing all seeds");
-			return response.status(200).send("seeds:");
+			const seeds = await knex<Seed>("seeds").select();
+			return response.status(200).json(seeds);
 		} catch (error) {
 			next(error);
 		}
