832fe80dc2cd9497ccbe69f6c6e8ec1100a027c0
flypapercut <flypapercut@proton.me>
2025-09-06 18:13:49 -0300

    feat(api) add creating seeds functionality

--- src/controllers/seeds.controller.ts
+++ src/controllers/seeds.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { knexConfig as knex } from "../database/knex";
 import { Seed } from "../models/Seed";
+import { v4 as uuidv4 } from "uuid";
 
 class SeedsController {
 	async index(request: Request, response: Response, next: NextFunction) {
@@ -8,6 +9,29 @@ class SeedsController {
 			console.log("indexing all seeds");
 			const seeds = await knex<Seed>("seeds").select();
 			return response.status(200).json(seeds);
+		} catch (error) {
+			next(error);
+		}
+	}
+
+	async create(request: Request, response: Response, next: NextFunction) {
+		try {
+			console.log("creating seed");
+			const { events, collected_date, name, mother_id, father_id } =
+				request.body;
+
+			const id = uuidv4();
+
+			await knex<Seed>("seeds").insert({
+				id,
+				mother_id,
+				father_id,
+				collected_date,
+				name,
+				events,
+			});
+
+			return response.status(201).json("");
 		} catch (error) {
 			next(error);
 		}
--- src/routes/seeds.routes.ts
+++ src/routes/seeds.routes.ts
@@ -7,10 +7,7 @@ const seedsController = new SeedsController();
 
 seedsRoutes.get("/", seedsController.index);
 seedsRoutes.get("/:id", (req, res) => console.log("seed"));
-seedsRoutes.post("/", (req, res) => {
-	console.log("creating seed");
-	return res.status(201).send("ok");
-});
+seedsRoutes.post("/", seedsController.create);
 seedsRoutes.put("/:id", () => console.log("updating seed: "));
 seedsRoutes.delete("/:id", () => console.log("deleting seed: "));
 
