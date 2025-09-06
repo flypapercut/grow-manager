738bbabe433437fbaa370ce4568294d29463fc0a
flypapercut <flypapercut@proton.me>
2025-09-06 16:49:01 -0300

    feat(db) add seed table migration

---
+++ src/database/migrations/20250906193804_create_seeds.ts
@@ -0,0 +1,18 @@
+import type { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+	await knex.schema.createTable("seeds", (t) => {
+		t.string("id").primary();
+		t.string("name").notNullable();
+		t.string("mother_id");
+		t.string("father_id");
+		t.string("collected_date");
+		t.jsonb("events");
+		t.timestamp("created_at").defaultTo(knex.fn.now());
+		t.timestamp("updated_at").defaultTo(knex.fn.now());
+	});
+}
+
+export async function down(knex: Knex): Promise<void> {
+	await knex.schema.dropTableIfExists("seeds");
+}
